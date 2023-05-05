99489a85f2ccab742df98001a908b22a5e257904
tomfactor <130264524+tomfactor@users.noreply.github.com>
2023-05-05 14:44:01 +0300

    Add files via upload

--- AnonX/plugins/tomp.py
+++ AnonX/plugins/tomp.py
@@ -7,7 +7,21 @@ from AnonX.core.call import Anon
 from AnonX.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall, TelegramServerError)
 
-audio_files = ["tom.mp3", "ahmed.mp3", "mas.mp3", "tomr.mp3"]
+
+
+async def play_audio_files(assistant, chat_id):
+    while True:
+        if not audio_files:
+            await assistant.leave_group_call(chat_id)
+            await message.reply("تم تشغيل جميع المقاطع بنجاح!")
+            break
+
+        audio_file = audio_files[0]
+        await assistant.join_group_call(chat_id, AudioPiped(f"./assets/{audio_file}"), stream_type=StreamType().pulse_stream)
+        await message.reply(f"تم تشغيل الملف الصوتي {audio_file} بنجاح!")
+
+        await asyncio.sleep(5)
+        audio_files.append(audio_files.pop(0))
 
 @app.on_message(filters.regex("بدا الالعاب"))
 async def start_playing(client, message):
@@ -16,34 +30,16 @@ async def start_playing(client, message):
         if "بدأ الألعاب" in message.text:
             await message.reply("جاري تشغيل الألعاب في الكول...")
 
-        audio_file = audio_files[0]
-        if "tom" in message.text:
-            audio_file = audio_files[0]
-        elif "ahmed" in message.text:
-            audio_file = audio_files[1]
-        elif "mas" in message.text:
-            audio_file = audio_files[2]
-        elif "tomr" in message.text:
-            audio_file = audio_files[3]
-
-        await assistant.join_group_call(message.chat.id, AudioPiped(f"./assets/{audio_file}"), stream_type=StreamType().pulse_stream)
-        await message.reply("تم التشغيل بنجاح!")
-
-        while True:
-            if not audio_files:
-                await assistant.leave_group_call(message.chat.id)
-                await message.reply("تم تشغيل جميع المقاطع بنجاح!")
-                break
-
-            await asyncio.sleep(5)
-            audio_files.append(audio_files.pop(0))
+        global audio_files
+        audio_files = ["tom.mp3", "ahmed.mp3", "mas.mp3", "tomr.mp3"]
+        await play_audio_files(assistant, message.chat.id)
 
     except NoActiveGroupCall:
         await message.reply("لا يوجد كول مفتوح يا معلم")
     except TelegramServerError:
         await message.reply("في مشكلة في السيرفر، ابعت الأمر تاني")
 
-@app.on_message(filters.regex("ايقاف التشغيل"))
+@app.on_message(filters.regex("استوب"))
 async def stop_playing(client, message):
     assistant = await group_assistant(Anon, message.chat.id)
     try:
@@ -51,4 +47,3 @@ async def stop_playing(client, message):
         await message.reply("تم الإيقاف بنجاح!")
     except NoActiveGroupCall:
         await message.reply("لا يوجد كول مفتوح يمعلم")
-
